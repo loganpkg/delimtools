2c4524f3e92f792703ca1f6a80904b8cd7768558
Logan McLintock <loganpkg@gmail.com>
2019-06-01 05:58:13 +0000

    insertfile

--- buf/spot.c
+++ buf/spot.c
@@ -38,16 +38,16 @@ if (ch == '\n') ++b->r; \
 } while (0)
 
 struct buf {
-  char *fn; /* Filename */
-  char *a; /* Array */
-  char *g; /* Start of gap */
-  char *c; /* Cursor */
-  char *m; /* Mark */
-  size_t s; /* Size of array */
-  size_t r; /* Cursor's row number */
-  size_t t; /* Row number of top of screen */
-  int m_set; /* If the mark is set */
-  int mod; /* If the buffer is modified */
+	char *fn;		/* Filename */
+	char *a;		/* Array */
+	char *g;		/* Start of gap */
+	char *c;		/* Cursor */
+	char *m;		/* Mark */
+	size_t s;		/* Size of array */
+	size_t r;		/* Cursor's row number */
+	size_t t;		/* Row number of top of screen */
+	int m_set;		/* If the mark is set */
+	int mod;		/* If the buffer is modified */
 };
 
 int safeadd(size_t * res, int num_args, ...)
@@ -71,83 +71,174 @@ int safeadd(size_t * res, int num_args, ...)
 	return 0;
 }
 
-int growgap(struct buf *b, size_t will_use) {
-  char *new_a = NULL;
-  size_t new_s;
-  size_t g_index = b->g - b->a;
-  size_t c_index = b->c - b->a;
+int growgap(struct buf *b, size_t will_use)
+{
+	char *new_a = NULL;
+	size_t new_s;
+	size_t g_index = b->g - b->a;
+	size_t c_index = b->c - b->a;
 
-  if (will_use <= b->c - b->g) return 0;
+	if (will_use <= b->c - b->g)
+		return 0;
 
-  if (safeadd(&new_s, 3, b->s, will_use, GAP)) {
-    LOG("safeadd failed");
-    return -1;
-  }
-  new_s -= b->c - b->g;
+	if (safeadd(&new_s, 3, b->s, will_use, GAP)) {
+		LOG("safeadd failed");
+		return -1;
+	}
+	new_s -= b->c - b->g;
 
-  if ((new_a = realloc(b->a, new_s)) == NULL) {
-    LOG("realloc failed");
-    return -1;
-  }
+	if ((new_a = realloc(b->a, new_s)) == NULL) {
+		LOG("realloc failed");
+		return -1;
+	}
 
-  memmove(new_a + c_index + new_s - b->s, new_a + c_index, b->s - c_index);
+	memmove(new_a + c_index + new_s - b->s, new_a + c_index,
+		b->s - c_index);
 
-  b->a = new_a;
-  b->g = new_a + g_index;
-  b->c = new_a + c_index;
-  b->s = new_s;
+	b->a = new_a;
+	b->g = new_a + g_index;
+	b->c = new_a + c_index;
+	b->s = new_s;
 
-  return 0;
+	return 0;
 }
 
-int insertch(struct buf *b, char ch) {
-  if (b->g == b->c) {
-    if (growgap(b, 1)) {
-      return -1;
-    }
-  }
+int insertch(struct buf *b, char ch)
+{
+	if (b->g == b->c) {
+		if (growgap(b, 1)) {
+			return -1;
+		}
+	}
 
-  b->m_set = 0;
-  b->mod = 1;
-  INSERT(b, ch);
-  return 0;
+	b->m_set = 0;
+	b->mod = 1;
+	INSERT(b, ch);
+	return 0;
 }
 
-int deletech(struct buf *b) {
-  char ch;
+int deletech(struct buf *b)
+{
+	char ch;
 
-  if (b->c == b->a + b->s - 1) return -1;
+	if (b->c == b->a + b->s - 1)
+		return -1;
 
-  b->m_set = 0;
-  b->mod = 1;
-  ch = *b->c;
-  ++b->c;
-  return ch;
+	b->m_set = 0;
+	b->mod = 1;
+	ch = *b->c;
+	++b->c;
+	return ch;
 }
 
-int leftch(struct buf *b) {
-  if (b->g == b->a) return -1;
+int leftch(struct buf *b)
+{
+	if (b->g == b->a)
+		return -1;
 
-  --b->g;
-  --b->c;
-  *b->c = *b->g;
+	--b->g;
+	--b->c;
+	*b->c = *b->g;
 
-  if (b->m_set && b->g == b->m) b->m = b->c;
+	if (b->m_set && b->g == b->m)
+		b->m = b->c;
 
-  if (*b->c == '\n') --b->r;
+	if (*b->c == '\n')
+		--b->r;
 
-  return *b->c;
+	return *b->c;
 }
 
-int rightch(struct buf *b) {
-  if (b->c == b->a + b->s - 1) return -1;
+int rightch(struct buf *b)
+{
+	if (b->c == b->a + b->s - 1)
+		return -1;
 
-  if (b->m_set && b->c == b->m) b->m = b->g;
+	if (b->m_set && b->c == b->m)
+		b->m = b->g;
 
-  if (*b->c == '\n') ++b->r;
+	if (*b->c == '\n')
+		++b->r;
 
-  *b->g = *b->c;
-  ++b->g;
-  ++b->c;
-  return *b->c;
+	*b->g = *b->c;
+	++b->g;
+	++b->c;
+	return *b->c;
+}
+
+int filesize(size_t * fs, char *fn)
+{
+	struct stat st;
+
+	if (fn == NULL) {
+		LOG("NULL filename");
+		return -1;
+	}
+
+	if (stat(fn, &st)) {
+		LOG("stat failed");
+		return -1;
+	}
+
+	if (!S_ISREG(st.st_mode)) {
+		LOG("Not regular file");
+		return -1;
+	}
+
+	if (st.st_size < 0) {
+		LOG("Negative file size");
+		return -1;
+	}
+
+	*fs = (size_t) st.st_size;
+	return 0;
+}
+
+int insertfile(stuct buf * b, char *fn)
+{
+	int ret = 0;
+	size_t fs;
+	FILE *fp = NULL;
+
+	if (filesize(&fs, fn)) {
+		LOG("filesize failed");
+		return -1;
+	}
+
+	if ((fp = fopen(fn, "r")) == NULL) {
+		LOG("fopen failed");
+		return -1;
+	}
+
+	if (growgap(b, fs)) {
+		LOG("growgap failed");
+		ret = -1;
+		goto clean_up;
+	}
+
+	if (fread(b->c - fs, 1, fs, fp) != fs) {
+		LOG("fread failed");
+		ret = -1;
+		goto clean_up;
+	}
+
+	if (ferror(fp)) {
+		LOG("fread error");
+		ret = -1;
+		goto clean_up;
+	}
+
+ clean_up:
+	if (fclose(fp)) {
+		LOG("fclose failed");
+		ret = -1;
+	}
+
+	if (!ret) {
+		b->c -= fs;
+		b->m_set = 0;
+		b->mod = 1;
+	}
+
+	return ret;
 }
