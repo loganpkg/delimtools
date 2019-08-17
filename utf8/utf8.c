4523721835b95073c1c8d8e38b9f42531defabf2
Logan McLintock <loganpkg@gmail.com>
2019-08-17 03:56:21 +0000

    introduced utf8chstr

--- utf8/utf8.c
+++ utf8/utf8.c
@@ -14,10 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <stdio.h>
+/* UTF8 functions */
+
+#include <stddef.h>
 #include "utf8.h"
 
-int ufreq(char *line, size_t len, size_t * cp_freq)
+int ucount(char *line, size_t len, size_t * cp_count)
 {
 	unsigned char uc;
 	uint32_t cp = 0;
@@ -29,28 +31,32 @@ int ufreq(char *line, size_t len, size_t * cp_freq)
 		uc = (unsigned char)line[i];
 
 		if (ISASCII(uc)) {
-			if (contin) {
+			if (contin)
 				return 1;
-			}
+
 			cp = uc;
+
 		} else if (ISLEAD2(uc)) {
-			if (contin) {
+			if (contin)
 				return 1;
-			}
+
 			cp = uc & 0x1F;
 			contin = 1;
+
 		} else if (ISLEAD3(uc)) {
-			if (contin) {
+			if (contin)
 				return 1;
-			}
+
 			cp = uc & 0x0F;
 			contin = 2;
+
 		} else if (ISLEAD4(uc)) {
-			if (contin) {
+			if (contin)
 				return 1;
-			}
+
 			cp = uc & 0x07;
 			contin = 3;
+
 		} else if (ISCONTIN(uc)) {
 			switch (contin) {
 			case 3:
@@ -67,41 +73,43 @@ int ufreq(char *line, size_t len, size_t * cp_freq)
 			return 1;
 		}
 
-		if (cp > MAXCP) {
+		if (cp > MAXCP)
 			return 1;
-		}
 
-		if (!contin) {
-			++cp_freq[cp];
-		}
+		if (!contin)
+			++cp_count[cp];
+
 	}
 
-	if (contin) {
+	if (contin)
 		return 1;
-	}
 
 	return 0;
 }
 
-int uprintcp(uint32_t cp)
+int ucptostr(uint32_t cp, char *utf8chstr)
 {
 	if (ISASCII(cp)) {
-		putchar(cp);
+		utf8chstr[0] = cp;
+		utf8chstr[1] = '\0';
 
 	} else if (ISSIZE2(cp)) {
-		putchar((cp >> 6 & 0x1F) | 0xC0);
-		putchar((cp & 0x3F) | 0x80);
+		utf8chstr[0] = (cp >>  6 & 0x1F) | 0xC0;
+		utf8chstr[1] = (cp       & 0x3F) | 0x80;
+		utf8chstr[2] = '\0';
 
 	} else if (ISSIZE3(cp)) {
-		putchar((cp >> 12 & 0x0F) | 0xE0);
-		putchar((cp >> 6 & 0x3F) | 0x80);
-		putchar((cp & 0x3F) | 0x80);
+		utf8chstr[0] = (cp >> 12 & 0x0F) | 0xE0;
+		utf8chstr[1] = (cp >>  6 & 0x3F) | 0x80;
+		utf8chstr[2] = (cp       & 0x3F) | 0x80;
+		utf8chstr[3] = '\0';
 
 	} else if (ISSIZE4(cp)) {
-		putchar((cp >> 18 & 0x07) | 0xF0);
-		putchar((cp >> 12 & 0x3F) | 0x80);
-		putchar((cp >> 6 & 0x3F) | 0x80);
-		putchar((cp & 0x3F) | 0x80);
+		utf8chstr[0] = (cp >> 18 & 0x07) | 0xF0;
+		utf8chstr[1] = (cp >> 12 & 0x3F) | 0x80;
+		utf8chstr[2] = (cp >>  6 & 0x3F) | 0x80;
+		utf8chstr[3] = (cp       & 0x3F) | 0x80;
+		utf8chstr[4] = '\0';
 
 	} else {
 		return 1;
