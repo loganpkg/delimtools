59ebe9d4da95ded87096cb43406be8d3516d00ac
Logan McLintock <loganpkg@gmail.com>
2019-08-17 04:24:44 +0000

    vertically aligned

--- utf8/utf8.h
+++ utf8/utf8.h
@@ -24,18 +24,18 @@
 #define ISASCII(u) ((u) < 0x80)
 
 #define ISCONTIN(uc) ((uc) >= 0x80 && (uc) < 0xC0)
-#define ISLEAD2(uc) ((uc) >= 0xC0 && (uc) < 0xE0)
-#define ISLEAD3(uc) ((uc) >= 0xE0 && (uc) < 0xF0)
-#define ISLEAD4(uc) ((uc) >= 0xF0 && (uc) < 0xF8)
-#define ISINVAL(uc) ((uc) >= 0xF8)
+#define ISLEAD2(uc)  ((uc) >= 0xC0 && (uc) < 0xE0)
+#define ISLEAD3(uc)  ((uc) >= 0xE0 && (uc) < 0xF0)
+#define ISLEAD4(uc)  ((uc) >= 0xF0 && (uc) < 0xF8)
+#define ISINVAL(uc)  ((uc) >= 0xF8)
 
 #define MAXCONTIN 3
 
 #define NUMCP 0x110000
 #define MAXCP (NUMCP - 1)
 
-#define ISSIZE2(cp) ((cp) >= 0x80 && (cp) < 0x800)
-#define ISSIZE3(cp) ((cp) >= 0x800 && (cp) < 0x10000)
+#define ISSIZE2(cp) ((cp) >= 0x80    && (cp) < 0x800)
+#define ISSIZE3(cp) ((cp) >= 0x800   && (cp) < 0x10000)
 #define ISSIZE4(cp) ((cp) >= 0x10000 && (cp) < NUMCP)
 
 int ucount(char *line, size_t len, size_t * cp_count);
