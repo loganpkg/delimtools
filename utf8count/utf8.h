de87ceb3eef091fbf287f35cab92b2bf61b0e850
Logan McLintock <loganpkg@gmail.com>
2019-10-07 09:53:48 +0000

    move UTF-8 code to generic library

--- utf8count/utf8.h
+++ utf8count/utf8.h
@@ -16,29 +16,5 @@
 
 /* UTF-8 header */
 
-#ifndef UTF8_H_
-#define UTF8_H_
 
 #include <stdint.h>
-
-#define ISASCII(u) ((u) < 0x80)
-
-#define ISCONTIN(uc) ((uc) >= 0x80 && (uc) < 0xC0)
-#define ISLEAD2(uc)  ((uc) >= 0xC0 && (uc) < 0xE0)
-#define ISLEAD3(uc)  ((uc) >= 0xE0 && (uc) < 0xF0)
-#define ISLEAD4(uc)  ((uc) >= 0xF0 && (uc) < 0xF8)
-#define ISINVAL(uc)  ((uc) >= 0xF8)
-
-#define MAXCONTIN 3
-
-#define NUMCP 0x110000
-#define MAXCP (NUMCP - 1)
-
-#define ISSIZE2(cp) ((cp) >= 0x80    && (cp) < 0x800)
-#define ISSIZE3(cp) ((cp) >= 0x800   && (cp) < 0x10000)
-#define ISSIZE4(cp) ((cp) >= 0x10000 && (cp) < NUMCP)
-
-int ucount(char *line, size_t len, size_t * cp_count);
-int ucptostr(uint32_t cp, char *utf8chstr);
-
-#endif
