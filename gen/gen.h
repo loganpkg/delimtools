fecc56ab0d437148bb6c97c06621ce9e69e3a22d
Logan McLintock <loganpkg@gmail.com>
2019-10-08 10:45:27 +0000

    UTF-8 code to own library

--- gen/gen.h
+++ gen/gen.h
@@ -27,6 +27,7 @@
 
 /* Error message */
 #define LOG(m) fprintf(stderr, "%s:%d: error: " m "\n", __FILE__, __LINE__)
+
 /* size_t addition overflow test */
 #define ADDOF(a, b)  ((a) > SIZE_MAX - (b))
 /* size_t multiplication overflow test */
@@ -63,27 +64,10 @@
 #define ESC 27
 #define Cqm 127
 
-#define ISASCII(u) ((u) < 0x80)
-
-#define ISCONTIN(uc) ((uc) >= 0x80 && (uc) < 0xC0)
-#define ISLEAD2(uc)  ((uc) >= 0xC0 && (uc) < 0xE0)
-#define ISLEAD3(uc)  ((uc) >= 0xE0 && (uc) < 0xF0)
-#define ISLEAD4(uc)  ((uc) >= 0xF0 && (uc) < 0xF8)
-#define ISINVAL(uc)  ((uc) >= 0xF8)
-
-#define MAXCONTIN 3
-
-#define NUMCP 0x110000
-#define MAXCP (NUMCP - 1)
-
-#define ISSIZE2(cp) ((cp) >= 0x80    && (cp) < 0x800)
-#define ISSIZE3(cp) ((cp) >= 0x800   && (cp) < 0x10000)
-#define ISSIZE4(cp) ((cp) >= 0x10000 && (cp) < NUMCP)
 
 int safeadd(size_t * res, int num_args, ...);
 int filesize(size_t * fs, char *fn);
-int ucount(char *line, size_t len, size_t * cp_count);
-int ucptostr(uint32_t cp, char *utf8chstr);
+
 int hextonum(int h, int *num);
 int strtochar(char *str, char *ch);
 
