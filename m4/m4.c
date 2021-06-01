313747873f7733d542b80bcf9b9ba6707712000a
Logan McLintock <loganpkg@gmail.com>
2021-06-01 12:35:00 +0000

    m4: increment

--- m4/m4.c
+++ m4/m4.c
@@ -38,6 +38,8 @@
 
 #define getch(b, read_stdin) (b->i ? *(b->a + --b->i) : (read_stdin ? getchar() : EOF))
 
+#define GAP_SIZE(b) (b->s - b->i)
+
 struct buf {
     char *a;
     size_t i;
@@ -63,7 +65,7 @@ struct mcall {
     char *name;                 /* Macro name */
     char *def;                  /* Macro definition before substitution */
     size_t bracket_depth;       /* Only unquoted brackets are counted */
-    size_t active_arg;          /* The current argument being collected */
+    size_t act_arg;             /* The current argument being collected */
     struct buf *arg_buf[10];    /* For argument collection */
 };
 
@@ -93,6 +95,9 @@ int grow_buf(struct buf *b, size_t will_use)
 {
     char *t;
     size_t new_s;
+    /* Gap is big enough, nothing to do */
+    if (will_use <= GAP_SIZE(b))
+        return 0;
     if (MOF(b->s, 2))
         return 1;
     new_s = b->s * 2;
@@ -136,7 +141,7 @@ int include(struct buf *b, char *fn)
 
     if (filesize(fn, &fs))
         return 1;
-    if (fs > b->s - b->i && grow_buf(b, fs))
+    if (fs > GAP_SIZE(b) && grow_buf(b, fs))
         return 1;
     if ((fp = fopen(fn, "r")) == NULL)
         return 1;
@@ -180,6 +185,7 @@ int getword(struct buf *token, struct buf *input, int read_stdin, int *err)
             *err = 1;
         return 1;
     }
+    /* EOF, no error */
     if (ungetch(token, x) == EOF)
         return 1;
 
@@ -195,19 +201,34 @@ int getword(struct buf *token, struct buf *input, int read_stdin, int *err)
             }
             if (!(isalnum(x) || x == '_')) {
                 /* Read past the end of the token, so put the char back */
-                if (ungetch(input, x) == EOF)
+                if (ungetch(input, x) == EOF) {
+                    *err = 1;
                     return 1;
+                }
                 break;
             } else {
                 /* Store the char */
-                if (ungetch(token, x) == EOF)
+                if (ungetch(token, x) == EOF) {
+                    *err = 1;
                     return 1;
+                }
             }
         }
     }
     /* Null terminate token */
-    if (ungetch(token, '\0') == EOF)
+    if (ungetch(token, '\0') == EOF) {
+        *err = 1;
         return 1;
+    }
+    return 0;
+}
+
+int ungetstr(struct buf *b, char *s)
+{
+    size_t len = strlen(s);
+    while (len)
+        if (ungetch(b, *(s + --len)) == EOF)
+            return 1;
     return 0;
 }
 
@@ -339,6 +360,8 @@ void free_mcall(struct mcall *m)
 {
     size_t j;
     if (m != NULL) {
+        free(m->name);
+        free(m->def);
         for (j = 1; j < 10; ++j)
             free_buf(*(m->arg_buf + j));
         free(m);
@@ -357,115 +380,352 @@ struct mcall *init_mcall(void)
             free_mcall(m);
             return NULL;
         }
-    m->active_arg = 1;
+    m->act_arg = 1;
     return m;
 }
 
+int stack_on_mcall(struct mcall **stack)
+{
+    struct mcall *t;
+    if ((t = init_mcall()) == NULL)
+        return 1;
+
+    /* Link new head on */
+    if (*stack != NULL)
+        t->next = *stack;
+
+    *stack = t;
+    return 0;
+}
+
+void delete_stack_head(struct mcall **stack)
+{
+    struct mcall *m;
+    if (*stack == NULL)
+        return;
+    m = (*stack)->next;
+    free_mcall(*stack);
+    *stack = m;
+}
+
+void free_stack(struct mcall *stack)
+{
+    struct mcall *m = stack, *next;
+    while (m != NULL) {
+        next = m->next;
+        free_mcall(m);
+        m = next;
+    }
+}
+
+int put_str(struct buf *b, char *s)
+{
+    size_t len = strlen(s);
+    if (len > GAP_SIZE(b) && grow_buf(b, len))
+        return 1;
+    memcpy(b->a + b->i, s, len);
+    b->i += len;
+    return 0;
+}
+
+int sub_args(struct buf *result, struct mcall *stack)
+{
+    int dollar_enc = 0;
+    char *s = stack->def, ch;
+    struct buf *b;
+    delete_buf(result);
+    while ((ch = *s++)) {
+        if (ch == '$') {
+            dollar_enc = 1;
+        } else if (dollar_enc && isdigit(ch) && ch != '0') {
+            b = *(stack->arg_buf + (ch - '0'));
+            if (b->i > GAP_SIZE(result) && grow_buf(result, b->i))
+                return 1;
+            memcpy(result->a + result->i, b->a, b->i);
+            result->i += b->i;
+            dollar_enc = 0;
+        } else {
+            if (dollar_enc && ungetch(result, '$') == EOF)
+                return 1;
+            if (ungetch(result, ch) == EOF)
+                return 1;
+        }
+    }
+    if (ungetch(result, '\0') == EOF)
+        return 1;
+    return 0;
+}
+
+int terminate_args(struct mcall *stack)
+{
+    size_t j;
+    for (j = 1; j < 10; j++)
+        if (ungetch(*(stack->arg_buf + j), '\0'))
+            return 1;
+    return 0;
+}
+
+#define QUIT do { \
+    ret = 1; \
+    goto clean_up; \
+} while (0)
+
 int main(int argc, char **argv)
 {
-    int ret = 0, read_stdin = 1, err, j, x;
-    struct buf *input = NULL, *token = NULL;
+    int ret = 0, read_stdin = 1, err, j;
+    struct buf *input = NULL, *token = NULL, *next_token = NULL, *result =
+        NULL;
     struct entry **ht = NULL, *e;
     int quote_on = 0;
-    size_t quote_depth = 0, fs, total_fs = 0;
+    size_t quote_depth = 0, fs, total_fs = 0, act_div = 0, k;
     /* Diversion 10 is -1 */
-    struct buf *diversion[11] = { NULL }, *act_div;
+    struct buf *diversion[11] = { NULL };
+    struct buf *output;
+    char left_quote[2] = { '[', '\0' };
+    char right_quote[2] = { ']', '\0' };
+    struct mcall *stack = NULL;
 
-    if ((input = init_buf()) == NULL) {
-        ret = 1;
-        goto clean_up;
-    }
+    if (argc < 1)
+        return 1;
 
-    if ((token = init_buf()) == NULL) {
-        ret = 1;
-        goto clean_up;
-    }
+    /* Setup buffers */
+    if ((input = init_buf()) == NULL)
+        QUIT;
+    if ((token = init_buf()) == NULL)
+        QUIT;
+    if ((next_token = init_buf()) == NULL)
+        QUIT;
+    if ((result = init_buf()) == NULL)
+        QUIT;
 
     /* Setup diversions */
-    for (j = 0; j < 11; ++j)
-        if ((*(diversion + j) = init_buf()) == NULL) {
-            ret = 1;
-            goto clean_up;
-        }
-    act_div = *diversion;
+    for (k = 0; k < 11; ++k)
+        if ((*(diversion + k) = init_buf()) == NULL)
+            QUIT;
+    output = *diversion;
 
-    if ((ht = calloc(HASH_TABLE_SIZE, sizeof(struct entry *))) == NULL) {
-        ret = 1;
-        goto clean_up;
-    }
+    if ((ht = calloc(HASH_TABLE_SIZE, sizeof(struct entry *))) == NULL)
+        QUIT;
 
     /* Define built-in macros. They have a def of NULL. */
-    if (upsert_entry(ht, "define", NULL)) {
-        ret = 1;
-        goto clean_up;
-    }
+    if (upsert_entry(ht, "define", NULL))
+        QUIT;
 
     if (argc > 1) {
+        /* Do not read stdin if there are command line files */
         read_stdin = 0;
         /* Get total size of command line files */
         for (j = argc - 1; j; --j) {
-            if (filesize(*(argv + j), &fs)) {
-                ret = 1;
-                goto clean_up;
-            }
+            if (filesize(*(argv + j), &fs))
+                QUIT;
             total_fs += fs;
         }
         /* Make sure buffer is big enough to hold all files */
-        if (grow_buf(input, total_fs)) {
-            ret = 1;
-            goto clean_up;
-        }
-
+        if (grow_buf(input, total_fs))
+            QUIT;
         /* Load command line files into buffer */
         for (j = argc - 1; j; --j)
-            if (include(input, *(argv + j))) {
-                ret = 1;
-                goto clean_up;
-            }
+            if (include(input, *(argv + j)))
+                QUIT;
     }
 
-    /* Read input word by word */
 
+/* Token string */
+#define TS token->a
 
-/*
-    err = 0;
-    while (!getword(token, input, read_stdin, &err)) {
+/* Next token string */
+#define NTS next_token->a
 
+/* End of argument collection */
+#define ARG_END (stack != NULL && stack->bracket_depth == 1 \
+    && !strcmp(TS, ")"))
 
+/* Nested close bracket (unquoted) */
+#define NESTED_CB (stack != NULL && stack->bracket_depth > 1 \
+    && !strcmp(TS, ")"))
 
+/* Nested open bracket (unquoted) */
+#define NESTED_OB (stack != NULL && !strcmp(TS, "("))
 
+/* Argument comma in a macro call */
+#define ARG_COMMA (stack != NULL && stack->bracket_depth == 1 \
+    && !strcmp(TS, ","))
+
+/* Token is a match of a macro name */
+#define TOKEN_MATCH ((isalpha(*TS) || *TS == '_') \
+    && (e = lookup_entry(ht, TS)) != NULL)
+
+/* Set output to stack argument collection buffer or diversion buffer */
+#define SET_OUTPUT output = (stack == NULL ? *(diversion + act_div) \
+    : *(stack->arg_buf + stack->act_arg))
+
+#define DIV(n) (*(diversion + n))
+
+#define OUT_DIV(n) do { \
+    if (DIV(n)->i && fwrite(DIV(n)->a, 1, DIV(n)->i, stdout) != DIV(n)->i) \
+        QUIT; \
+    DIV(n)->i = 0; \
+} while (0)
+
+/* Tests if a string consists of a single whitespace character */
+#define WS(s) (!strcmp(s, " ") || !strcmp(s, "\t") || !strcmp(s, "\n") \
+    || !strcmp(s, "\r"))
+
+/* Remove stack head and set ouput */
+#define REMOVE_SH do { \
+    delete_stack_head(&stack); \
+    SET_OUTPUT; \
+} while (0)
+
+/* Stack macro collected argument number n */
+#define ARG(n) (*(stack->arg_buf + n))->a
+
+#define READ_TOKEN(t) do { \
+    err = 0; \
+    if (getword(t, input, read_stdin, &err)) { \
+        if (err) \
+            QUIT; \
+        else \
+            goto end_of_input; \
+    } \
+} while(0)
+
+#define EAT_WS do { \
+    do \
+        READ_TOKEN(next_token); \
+    while (WS(NTS)); \
+    if (ungetstr(input, NTS)) QUIT; \
+} while (0)
+
+    /* m4 loop: read input word by word */
+  top:
+    /* Write stdout buffer (diversion 0) for interactive use */
+    OUT_DIV(0);
+    /* Read token */
+    READ_TOKEN(token);
+
+    if (!strcmp(TS, left_quote)) {
+        if (!quote_on)
+            quote_on = 1;
+        if (quote_depth && put_str(output, TS))
+            QUIT;
+        ++quote_depth;
+        goto top;
     }
-    if (err) {
-        ret = 1;
-        goto clean_up;
+
+    if (!strcmp(TS, right_quote)) {
+        if (quote_depth > 1 && put_str(output, TS))
+            QUIT;
+        if (!--quote_depth)
+            quote_on = 0;
+        goto top;
     }
-*/
 
+    if (quote_on) {
+        if (put_str(output, TS))
+            QUIT;
+        goto top;
+    }
 
-    upsert_entry(ht, "logan", "cool");
+    if (TOKEN_MATCH) {
+        /* Token match */
+        READ_TOKEN(next_token);
 
-    printf("%s\n", get_def(ht, "logan"));
+        if (!strcmp(NTS, "(")) {
+            /* Start of macro */
+            /* Add macro call to stack */
+            if (stack_on_mcall(&stack))
+                QUIT;
+            /* Copy macro name */
+            if ((stack->name = strdup(TS)) == NULL)
+                QUIT;
+            /* Copy macro definition (built-ins will be NULL) */
+            if (e->def != NULL && (stack->def = strdup(e->def)) == NULL)
+                QUIT;
+            /* Increment bracket depth for this first bracket */
+            ++stack->bracket_depth;
+            SET_OUTPUT;
+            EAT_WS;
+        } else {
+            /* All macro calls must have arguments */
+            /* Put the next token back into the input */
+            if (ungetstr(input, NTS))
+                QUIT;
+            /* Send token to output */
+            if (put_str(output, TS))
+                QUIT;
+        }
+        goto top;
+    }
 
-    delete_entry(ht, "logan");
+    if (ARG_END) {
+        /* End of argument collection */
+        /* Decrement bracket depth for bracket just encountered */
+        --stack->bracket_depth;
+        if (stack->def == NULL && !strcmp(stack->name, "define")) {
+            /* Define built-in macro */
+            if (terminate_args(stack))
+                QUIT;
+            if (upsert_entry(ht, ARG(1), ARG(2)))
+                QUIT;
+        } else if (stack->def != NULL) {
+            /* User defined macro */
+            if (sub_args(result, stack))
+                QUIT;
+            if (ungetstr(input, result->a))
+                QUIT;
+        }
+        REMOVE_SH;
+        goto top;
+    }
 
-    printf("%s\n", get_def(ht, "logan"));
+    if (ARG_COMMA) {
+        if (stack->act_arg == 9) {
+            fprintf(stderr, "Macro call has too many arguments\n");
+            QUIT;
+        }
+        ++stack->act_arg;
+        SET_OUTPUT;
+        EAT_WS;
+        goto top;
+    }
 
-    delete_entry(ht, "logan");
-    delete_entry(ht, "logan");
-    delete_entry(ht, "logan");
+    if (NESTED_CB) {
+        if (put_str(output, TS))
+            QUIT;
+        --stack->bracket_depth;
+        /* Do we want to eat whitespace here? */
+        goto top;
+    }
 
-    upsert_entry(ht, "logan", "cool");
+    if (NESTED_OB) {
+        if (put_str(output, TS))
+            QUIT;
+        ++stack->bracket_depth;
+        /* Do we want to eat whitespace here? */
+        goto top;
+    }
 
-    printf("%s\n", get_def(ht, "logan"));
+    /* Pass through token */
+    if (put_str(output, TS)) {
+        QUIT;
+        goto top;
+    }
+    goto top;
 
+  end_of_input:
+
+    for (k = 0; k < 10; k++)
+        OUT_DIV(k);
 
   clean_up:
     free_buf(input);
     free_buf(token);
-    for (j = 0; j < 11; ++j)
-        free_buf(*(diversion + j));
-
+    free_buf(next_token);
+    free_buf(result);
+    for (k = 0; k < 11; ++k)
+        free_buf(*(diversion + k));
     free_hash_table(ht);
-
+    free_stack(stack);
     return ret;
 }
